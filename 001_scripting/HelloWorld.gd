89dd5192e95e06f184f0bf55eee7c6c38a3ee09f
Fabio Z Tessitore <fabioztessitore@gmail.com>
2020-04-10 17:45:45 +0200

    scripting

---
+++ 001_scripting/HelloWorld.gd
@@ -0,0 +1,12 @@
+extends Panel
+
+# il codice e' scritto nel pannello cosi'
+# possiamo accedere contemporaneamente a
+# button e label
+
+func _on_Button_pressed():
+	$MarginContainer/CenterContainer/VBoxContainer/CenterContainer/Label.text = "Hello, World!"
+	$MessageTimer.start()
+
+func _on_MessageTimer_timeout():
+	$MarginContainer/CenterContainer/VBoxContainer/CenterContainer/Label.text = "Premi il pulsante per cambiare il testo"
---
+++ README.md
@@ -0,0 +1,4 @@
+# Learning Godot
+
+1. Scripting
+Creazione di un pannello (Panel) contenente una Label e un Button. Alla pressione del Button la Label cambia testo. Un Timer riporta il testo al valore inziale.
